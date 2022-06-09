0f3eef270d22913bc2d3f5797407ddf51be46a21
Tuncay YAYLALI <81981206+tuncayyaylali@users.noreply.github.com>
2022-06-09 22:34:09 +0300

    Create Advanced-Aritmetics.py

---
+++ Advanced-Aritmetics.py
@@ -0,0 +1,19 @@
+# AdvancedArithmetic
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        liste=[]
+        for i in range(1, n+1):
+            if n%i==0:
+                liste.append(i)
+        return sum(liste)
+
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)

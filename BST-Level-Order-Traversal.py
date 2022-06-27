5a77a9d54b6e3afc3299baf86b46faee297fbac4
Tuncay YAYLALI <81981206+tuncayyaylali@users.noreply.github.com>
2022-06-27 11:28:34 +0300

    Create BST-Level-Order-Traversal.py

---
+++ BST-Level-Order-Traversal.py
@@ -0,0 +1,33 @@
+import sys
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def levelOrder(self,root):
+        #Write your code here
+        queue = [root]
+        for node in filter(None, queue):
+            print(node.data, end= ' ')
+            queue += [node.left, node.right]        
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)
